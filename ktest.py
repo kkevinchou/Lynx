2931282df03979a7c86444c3623d15da490491e4
Kevin C <kkevinchou@gmail.com>
2014-05-15 17:05:59 +0400

    removed debug code

--- ktest.py
+++ ktest.py
@@ -9,30 +9,24 @@ from lib.geometry.polygon import Polygon
 
 dude = Polygon([Vec2d(1, 1), Vec2d(1, 0), Vec2d(0, 0), Vec2d(0, 1)])
 
-# obstacle1 = generate_random_polygon(100, 50, 200, 200, 10)
-# obstacle2 = generate_random_polygon(100, 201, 200, 300, 10)
-# obstacle3 = generate_random_polygon(100, 301, 200, 500, 10)
-# obstacle4 = generate_random_polygon(300, 201, 500, 300, 10)
-# obstacle5 = generate_random_polygon(700, 401, 800, 600, 10)
-# obstacle6 = generate_random_polygon(500, 401, 600, 600, 10)
+obstacle1 = generate_random_polygon(100, 50, 200, 200, 10)
+obstacle2 = generate_random_polygon(100, 201, 200, 300, 10)
+obstacle3 = generate_random_polygon(100, 301, 200, 500, 10)
+obstacle4 = generate_random_polygon(300, 201, 500, 300, 10)
+obstacle5 = generate_random_polygon(700, 401, 800, 600, 10)
+obstacle6 = generate_random_polygon(500, 401, 600, 600, 10)
 
-vertex_lists = [
-    [Vec2d(540, 591), Vec2d(600, 571), Vec2d(590, 440), Vec2d(542, 471), Vec2d(533, 576)]
-]
-
-# obstacles = [obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6]
-obstacles = create_polygons(vertex_lists)
+obstacles = [obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6]
 
 planner = AStarPlanner()
 planner.add_polygons(obstacles)
 planner.init()
 
-start_pos = (596, 526)
-goal_pos = (462, 516)
+start_pos = (0, 0)
 renderer = Renderer(800, 600)
-path = planner.find_path(start_pos[0], start_pos[1], goal_pos[0], goal_pos[1])
 
 position = Vec2d(start_pos[0], start_pos[1])
+path = []
 path_index = 0
 speed = 5
 
