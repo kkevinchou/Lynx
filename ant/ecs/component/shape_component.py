a38670b9034d205f19a7c8caac7e7de122b9c14d
Kevin C <kkevinchou@gmail.com>
2014-06-07 16:53:00 +0900

    get_points() for shape_component

--- ant/ecs/component/shape_component.py
+++ ant/ecs/component/shape_component.py
@@ -8,3 +8,7 @@ class ShapeComponent(Component):
     def __init__(self, x_min, y_min, x_max, y_max, max_points, entity):
         self.polygon = generate_random_polygon(x_min, y_min, x_max, y_max, max_points)
         self.entity = entity
+        self.points = [self.entity.position + point for point in self.polygon.get_points()]
+
+    def get_points(self):
+        return self.points
